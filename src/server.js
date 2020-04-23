ca3f189f4094e1a6c43fac1fd50569aa8e5807f9
DeepakJayabal <deepakjayabal@fico.com>
2020-04-23 22:49:11 +0530

    go on

--- src/server.js
+++ src/server.js
@@ -11,7 +11,13 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: true
+  })
+);
+app.options('*', cors());
 app.use(cookieParser());
 
 app.get('/example', async (req, res) => {
